6b74f9fe52d04421f72f68a890570a35587302e9
Mihai <mcci0023@scs.ubbcluj.ro>
2021-03-16 20:12:36 +0200

    Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,33 @@
 package com.company;
 import java.util.*;
 /*Task
-Complete the code in the editor below. Need to:
-
-    1. Declare 3 variables: one of type int, one of type double, and one of type String.
-    2. Read 3 lines of input from stdin and initialize your variables.
-    3. Use the + operator to perform the following operations:
-
-    a. Print the sum of "i" plus your int variable on a new line.
-    b. Print the sum of "d" plus your double variable to a scale of one decimal place on a new line.
-    c. Concatenate "s" with the string you read as input and print the result on a new line. *///Expand to see the task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added
+as tip), and tax percent (the percentage of the meal price being added as tax) for a meal,
+find and print the meal's total cost.
+Round the result to the nearest integer. *///Expand to see the task
 
 
 
 public class Main {
 
     public static void main(String[] args) {
-        int i = 4;
-        double d = 4.0;
-        String s = "Hello OOP ";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the meal cost ");
+        double meal_cost = in.nextDouble();
+        System.out.println("Enter the tip percent");
+        int tip_percent = in.nextInt();
+        System.out.println("Enter the tax percent");
+        int tax_percent = in.nextInt();
+        in.close();
 
-        Scanner scan = new Scanner(System.in);
+        double a = (tip_percent * meal_cost) / 100;
+        double b = (tax_percent * meal_cost) / 100;
+        double c = meal_cost + a + b;
 
-        int a = scan.nextInt();
-        double b = scan.nextDouble();
-        scan.nextLine();
-        String c = scan.nextLine();
-
-        a += i;
-        b += d;
-        s += c;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(s);
-
-        scan.close();
+        int totalCost = (int) Math.round(c);
+        System.out.println("The total meal cost " + totalCost + " $");
 
     }
 }
+
 
